228b8f212b80bf171de77e22f61f50749e984fca
Arthur Silveira <144953268+IsArthurSilveira@users.noreply.github.com>
2024-09-19 18:18:32 +0000

    AtividadeConcluida

--- tiposPrimitivos.js
+++ tiposPrimitivos.js
@@ -15,12 +15,12 @@ let num1 = parseFloat(prompt("Digite um numero : "));
 let num2 = parseFloat(prompt ("Digite outro numero : "));
 const soma = num1+num2;
 const sub = num1-num2;
-const mult = num1*num2;
+const mult = (num1*num2).toFixed(2);
 const divi= (num1/num2).toFixed(2);
-console.log(`3° A) - Somando os numeros : ${num1} e ${num2} temos o número ${soma}`);
-console.log(`B) - Subtraindo os numeros : ${num1} e ${num2} temos o número ${sub}`);
-console.log(`C) - Multiplicando os numeros : ${num1} e ${num2} temos o número ${mult}`);
-console.log(`C) - Dividindo os numeros : ${num1} e ${num2} temos o número ${divi}`);
+console.log(`Somando os numeros : ${num1} e ${num2} temos o número ${soma}`);
+console.log(`Subtraindo os numeros : ${num1} e ${num2} temos o número ${sub}`);
+console.log(`Multiplicando os numeros : ${num1} e ${num2} temos o número ${mult}`);
+console.log(`Dividindo os numeros : ${num1} e ${num2} temos o número ${divi}`);
 // 4 questão
 console.log("Agora vamos calcular a área de um Triangulo");
 let base = parseFloat(prompt("Digite o valor da base do Triangulo : "));
@@ -45,7 +45,7 @@ while(true){
 }
 // 6 questão
 let produto = parseFloat(prompt("Digite o valor do produto : "));
-let desconto = 0.2;
+let desconto = 0.5;
 const  valorDesconto = produto * desconto;
 const  valorFinal = produto - valorDesconto;
 console.log(`O valor do desconto é R$${valorDesconto} e o valor final do produto é R$${valorFinal}`);
@@ -57,35 +57,35 @@ if(salario<=1903.98){
     console.log("Você está isento de pagar imposto de renda");
 }
 else if(salario<=2826.65){
-    ir = (salario*0.075)-142.80;
+    ir = ((salario*0.075)-142.80).toFixed(2);
     console.log(`Você deve pagar R$${ir} de imposto de renda`);
 }
 else if(salario<=3751.05){
-    ir = (salario*0.15)-354.80;
+    ir = ((salario*0.15)-354.80).toFixed(2);
     console.log(`Você deve pagar R$${ir} de imposto de renda`);
 }
 else if(salario<=4664.68){
-    ir = (salario*0.225)-636.13;
+    ir = ((salario*0.225)-636.13).toFixed(2);
     console.log(`Você deve pagar R$${ir} de imposto de renda`);
 }
 else{
-    ir = (salario*0.275)-869.36;
+    ir = ((salario*0.275)-869.36).toFixed(2);
     console.log(`Você deve pagar R$${ir} de imposto de renda`);
 }
 // 8 questão
 let valorDolar = parseFloat(prompt("Digite o valor em dólar : "));
 let cotacao = 5.25;
-let valorReal = valorDolar * cotacao;
+let valorReal = (valorDolar * cotacao).toFixed(2);
 console.log(`O valor em dólar é $${valorDolar} e convertendo para real temos R$${valorReal}`);
 // 9 questão
-var celsius = 37;
-var fahrenheit = ((celsius * 9)/5) + 32;
-console.log(`9° - A temperatura é ${celsius}°C, convertendo para Fahrenheit temos ${fahrenheit} °F`);
+let celsius = parseFloat(prompt("Digite a temperatura em Celsius : "));
+const fahrenheit = ((celsius * 9)/5) + 32;
+console.log(`A temperatura é ${celsius}°C, convertendo para Fahrenheit temos ${fahrenheit} °F`);
 // 10 questão
 let peso = parseFloat(prompt("Digite o seu peso : "));
 let altura2 = parseFloat(prompt("Digite a sua altura : "));
-let imc = peso/(altura2*altura2);
-console.log(`O seu IMC é ${imc.toFixed(2)}`);
+let imc = (peso/(altura2*altura2)).toFixed(2);
+console.log(`O seu IMC é ${imc}`);
 if(imc<18.5){
     console.log("Você está abaixo do peso");
 }
